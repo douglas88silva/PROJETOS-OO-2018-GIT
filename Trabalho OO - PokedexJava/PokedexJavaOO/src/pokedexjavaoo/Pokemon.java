23f17defd5db3684063474ed5cc2d7397b948982
ice <ice@lab06m06.nrc.ice.ufjf.br>
2018-10-26 20:24:54 -0300

    .

--- Trabalho OO - PokedexJava/PokedexJavaOO/src/pokedexjavaoo/Pokemon.java
+++ Trabalho OO - PokedexJava/PokedexJavaOO/src/pokedexjavaoo/Pokemon.java
@@ -9,19 +9,34 @@ package pokedexjavaoo;
  *
  * @author HomeNot
  */
-public class Pokemon {
+public class Pokemon extends TipoPokemon {
     
     private String nome;
     private String apelido;
     private int dust;
-    
     private int id;
+    private TipoPokemon tipoPokemon;
     
     public Pokemon(String nome,int dust){
         
         try{
-            //buscar o nome na base de dados 
-        
+            //buscar o nome na base de dados
+            
+            
+            //Buscar os tipos na base//criar uma execao caso os dois tipos sejam null
+            String tipo1 = null,tipo2 = null;
+            
+            if(tipo1 !=null && tipo2 !=null)
+            {
+               System.out.println("Não foi possivel encontrar o tipo do seu pokemon na base");
+
+            }
+            
+            else{
+                this.tipoPokemon = new TipoPokemon(tipo1,tipo2);
+                
+            }
+            
         }catch(Exception e)
         {
             System.out.println("Pokemon não existe na base de dados");
---
+++ Trabalho OO - PokedexJava/PokedexJavaOO/src/pokedexjavaoo/TipoPokemon.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokedexjavaoo;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ice
+ */
+public class TipoPokemon implements AtaquesTipo{
+    
+   private String tipo1 = null;
+   private String tipo2 = null;
+   private String ataqueRapido;
+   private String AtaqueCarregado;
+  
+   
+   public TipoPokemon(){
+   }
+   
+    public TipoPokemon(String tipo1, String tipo2) {
+        this.tipo1 = tipo1;
+        this.tipo2 = tipo2;      
+        
+               
+        this.setAtaque(tipo1);
+        this.setAtaque(tipo2);
+    }
+   
+    @Override
+    public void setAtaque(String tipo) {
+        
+        if(tipo != null)
+        {
+          //busca os melhores ataques na base
+            
+            
+            this.ataqueRapido = " ";
+            this.AtaqueCarregado = " ";
+            
+        }
+         
+    }
+   
+   
+}
